80dbccef322ea3f9eac2fecf76c9e29198d2c5d4
Steph Scully <stephanielarascullion@yahoo.com>
2019-05-24 14:34:05 +0100

    MVP completed! Ready to move onto extensions

--- Room.rb
+++ Room.rb
@@ -8,4 +8,31 @@ attr_reader(:name, :song_playlist, :guests)
     @guests = guests
   end
 
+  def check_in_guest(guest)
+    @guests << guest
+  end
+
+#check in multiple guests at once?
+  def check_in_guests(guests)
+      # for each guest I want to check in (guests)
+      # add the guest I'm on in the loop to the @guests array
+      guests.each do |guest|
+        @guests << guest
+      end
+  end
+
+  def check_out_guest(guest)
+    @guests.delete(guest)
+  end
+
+  def add_song_to_room(new_song)
+    @song_playlist << new_song
+  end
+
+  def add_songs_to_room(new_songs)
+    new_songs.each do |song|
+      @song_playlist << song
+    end
+  end
+
 end
--- Specs/Room_Specs.rb
+++ Specs/Room_Specs.rb
@@ -8,21 +8,24 @@ class TestRoom  < MiniTest::Test
 
   def setup
 
-    @song1 = Song.new("Personal Jeses", "Depeche Mode")
+    @song1 = Song.new("Personal Jesus", "Depeche Mode")
     @song2 = Song.new("Mardie Bum", "Artic Monkeys")
     @song3 = Song.new("Disturbia", "Rhianna")
     @song4 = Song.new("Into the Groove", "Madonna")
     @song5 = Song.new("Fake Empire", "The National")
     @song6 = Song.new("Uptown Funk", "Mark Ronson")
-
-    @room1 = Room.new("80's Dance", [@song1, @song4], [])
-    @room2 = Room.new("Indie and Chill", [@song2, @song5], [])
-    @room3 = Room.new("Mainstrem Pop", [@song3, @song6], [])
+    @song7 = Song.new("Blue Monday", "New Order")
+    @song8 = Song.new("Toxic", "Britney Spears")
+    @song9 = Song.new("Shake it Off", "Taylor Swift")
 
     @guest1 = Guest.new("Alice")
     @guest2 = Guest.new("Ross")
     @guest3 = Guest.new("Matthew")
     @guest4 = Guest.new("Leah")
+
+    @room1 = Room.new("80's Dance", [@song1, @song4], [])
+    @room2 = Room.new("Indie and Chill", [@song2, @song5], [@guest1, @guest2, @guest3, @guest4])
+    @room3 = Room.new("Mainstrem Pop", [@song3,], [])
 
   end
 
@@ -31,7 +34,35 @@ class TestRoom  < MiniTest::Test
   end
 
   def test_song_playlist
-    assert_equal([@song3,@song6], @room3.song_playlist)
+    assert_equal([@song3], @room3.song_playlist)
+  end
+
+  def test_check_in_guest
+    @room1.check_in_guest(@guest4)
+    assert_equal([@guest4], @room1.guests)
+  end
+
+#multiple at once
+  def test_check_in_guests
+    @room1.check_in_guests([@guest4, @guest1])
+    assert_equal([@guest4, @guest1], @room1.guests)
+  end
+
+  def test_check_out_guest
+  @room2.check_out_guest(@guest2)
+  assert_equal([@guest1, @guest3, @guest4], @room2.guests)
+  end
+
+  def test_add_song_to_room
+    expected = [@song1, @song4, @song7]
+    assert_equal(expected, @room1.add_song_to_room(@song7))
+  end
+
+#multiple at once
+  def test_add_songs_to_room
+    @room3.add_songs_to_room([@song6, @song8, @song9])
+    expected = [@song3, @song6, @song8, @song9]
+    assert_equal(expected, @room3.song_playlist)
   end
 
 end
